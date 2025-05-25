a211e60787c7b94d201b61276b178d042ae7c6d3
cipherrr <135461482+cipherrr@users.noreply.github.com>
2025-05-25 02:55:32 -0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -12,7 +12,7 @@ select_drive() {
 		echo
 
 		if [ "$drive" = "q" ]; then
-			echo 'Canceled drive selection.'
+			echo "Canceled drive selection."
 			echo
 			return 1
 		fi
@@ -39,15 +39,15 @@ format_drive() {
 	parted -s /dev/$drive mklabel gpt
 	parted -s mkpart "EFI System Partition" fat32 1MiB 1GiB set 1 esp on
 	parted -s mkpart "Root Partition" ext4 1GiB 100% type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
-	mkfs.fat -F 32 /dev/$drive'p1'
-	mkfs.ext4 /dev/$drive'p2'
+	mkfs.fat -F 32 /dev/$drive"p1"
+	mkfs.ext4 /dev/$drive"p2"
 }
 
 mount_drive() {
 	select_drive
 	[ $? -eq 1 ] && return
-	mount --mkdir /dev/$drive'p2' /mnt
-	mount --mkdir /dev/$drive'p1' /mnt/boot
+	mount --mkdir /dev/$drive"p2" /mnt
+	mount --mkdir /dev/$drive"p1" /mnt/boot
 }
 
 configure_pacman() {
@@ -67,17 +67,17 @@ change_root() {
 
 superuser() {
 	mkdir -p /etc/sudoers.d
-	echo '%wheel ALL=(ALL:ALL) ALL' > /etc/sudoers.d/01_ag
-	echo 'ag ALL=(ALL:ALL) NOPASSWD: /usr/bin/nvidia-smi' >> /etc/sudoers.d/01_ag
-	echo 'ag ALL=(ALL:ALL) NOPASSWD: /usr/bin/nvidia-settings' >> /etc/sudoers.d/01_ag
+	echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/01_ag
+	echo "ag ALL=(ALL:ALL) NOPASSWD: /usr/bin/nvidia-smi" >> /etc/sudoers.d/01_ag
+	echo "ag ALL=(ALL:ALL) NOPASSWD: /usr/bin/nvidia-settings" >> /etc/sudoers.d/01_ag
 }
 
 autologin() {
 	mkdir -p /etc/systemd/system/getty@tty1.service.d
-	echo '[Service]' > /etc/systemd/system/getty@tty1.service.d/autologin.conf
-	echo 'Type=simple' >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
-	echo 'ExecStart=' >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
-	echo 'ExecStart=/bin/agetty --autologin ag %I $TERM' >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+	echo "[Service]" > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+	echo "Type=simple" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+	echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+	echo "ExecStart=/bin/agetty --autologin ag %I $TERM" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
  	systemctl enable getty@tty1.service
 }
 
@@ -91,17 +91,17 @@ nvidia_oc() {
 
 xorg_as_root() {
 	mkdir -p /etc/X11
-	echo 'needs_root_rights = yes' > /etc/X11/Xwrapper.config
+	echo "needs_root_rights = yes" > /etc/X11/Xwrapper.config
 }
 
 tcp_fastopen() {
 	mkdir -p /etc/sysctl.d
-	echo 'net.ipv4.tcp_fastopen = 3' > /etc/sysctl.d/10-network.conf
+	echo "net.ipv4.tcp_fastopen = 3" > /etc/sysctl.d/10-network.conf
 }
 
 disable_coredump() {
 	mkdir -p /etc/sysctl.d
-	echo 'kernel.core_pattern=|/bin/false' > /etc/sysctl.d/50-coredump.conf
+	echo "kernel.core_pattern=|/bin/false" > /etc/sysctl.d/50-coredump.conf
 }
 
 fstrim() {
@@ -112,28 +112,29 @@ zram() {
 	mkdir -p /etc/udev/rules.d
 	echo 'ACTION=="add", KERNEL=="zram0", ATTR{initstate}=="0", ATTR{comp_algorithm}="lz4", ATTR{disksize}="16G", RUN="/usr/bin/mkswap -U clear %N", TAG+="systemd"' > /etc/udev/rules.d/99-zram.rules
 
-  	if grep -q '/dev/zram0' /etc/fstab; then
-   		echo '#/dev/zram0 none swap defaults,discard,pri=100 0 0' >> /etc/fstab
+  	if grep -q "/dev/zram0" /etc/fstab; then
+   		echo "#/dev/zram0 none swap defaults,discard,pri=100 0 0" >> /etc/fstab
      		return
 	fi
-	echo '/dev/zram0 none swap defaults,discard,pri=100 0 0' >> /etc/fstab
+	echo "/dev/zram0 none swap defaults,discard,pri=100 0 0" >> /etc/fstab
 }
 
 locales() {
 	ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
-	echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+	echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 	locale-gen
-	echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+	echo "LANG=en_US.UTF-8" > /etc/locale.conf
 	systemctl enable systemd-timesyncd
 }
 
 network() {
 	mkdir -p /etc/systemd/network
-	echo '[Match]' > /etc/systemd/network/20-wired.network
-	echo 'Name=e*' >> /etc/systemd/network/20-wired.network
-	echo '' >> /etc/systemd/network/20-wired.network
-	echo '[Network]' >> /etc/systemd/network/20-wired.network
-	echo 'DHCP=yes' >> /etc/systemd/network/20-wired.network
+	echo "[Match]" > /etc/systemd/network/20-wired.network
+	echo "Name=enp5s0" >> /etc/systemd/network/20-wired.network
+	echo >> /etc/systemd/network/20-wired.network
+	echo "[Network]" >> /etc/systemd/network/20-wired.network
+	echo "DHCP=yes" >> /etc/systemd/network/20-wired.network
+ 	echo "DNS=1.1.1.1" >> /etc/systemd/network/20-wired.network
 	systemctl enable systemd-networkd systemd-resolved
 }
 
@@ -150,14 +151,14 @@ generate_images() {
 bootloader() {
 	bootctl install
 	mkdir -p /boot/loader/entries
-	echo 'default @saved' > /boot/loader/loader.conf
-	echo 'timeout menu-force' >> /boot/loader/loader.conf
-	echo 'console-mode max' >> /boot/loader/loader.conf
+	echo "default @saved" > /boot/loader/loader.conf
+	echo "timeout menu-force" >> /boot/loader/loader.conf
+	echo "console-mode max" >> /boot/loader/loader.conf
 
-	echo 'title   Arch Linux' > /boot/loader/entries/arch.conf
-	echo 'linux   /vmlinuz-linux' >> /boot/loader/entries/arch.conf
+	echo "title   Arch Linux" > /boot/loader/entries/arch.conf
+	echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 	echo "initrd  /$ucode.img" >> /boot/loader/entries/arch.conf
-	echo 'initrd  /booster-linux.img' >> /boot/loader/entries/arch.conf
+	echo "initrd  /booster-linux.img" >> /boot/loader/entries/arch.conf
 	echo "options root=UUID=$(findmnt / -o UUID -n) rw quiet zswap.enabled=0" >> /boot/loader/entries/arch.conf
 }
 
@@ -165,10 +166,10 @@ windows_dualboot() {
 	pacman -Sy --needed --noconfirm edk2-shell
 	cp /usr/share/edk2-shell/x64/Shell.efi /boot/shellx64.efi
 	mkdir -p /boot/loader/entries
-	echo 'title   Windows' > /boot/loader/entries/windows.conf
-	echo 'efi     /shellx64.efi' >> /boot/loader/entries/windows.conf
-	echo 'options -nointerrupt -nomap -noversion -exit -noconsoleout' >> /boot/loader/entries/windows.conf
-	echo 'HD0b:EFI\Microsoft\Boot\Bootmgfw.efi' >> /boot/loader/entries/windows.conf
+	echo "title   Windows" > /boot/loader/entries/windows.conf
+	echo "efi     /shellx64.efi" >> /boot/loader/entries/windows.conf
+	echo "options -nointerrupt -nomap -noversion -exit -noconsoleout" >> /boot/loader/entries/windows.conf
+	echo "HD0b:EFI\Microsoft\Boot\Bootmgfw.efi" >> /boot/loader/entries/windows.conf
 }
 
 configure() {
@@ -190,22 +191,22 @@ configure() {
 }
 
 while true; do
-	echo '+-------------------+'
-	echo '| ARCH LINUX HELPER |'
-	echo '+-------------------+'
-	echo '1. Format Drive'
-	echo '2. Mount Drive'
-	echo '3. Install ArchLinux'
-	echo '4. Change Root'
-	echo '5. Configure'
-	echo '6. Exit'
+	echo "+-------------------+"
+	echo "| ARCH LINUX HELPER |"
+	echo "+-------------------+"
+	echo "1. Format Drive"
+	echo "2. Mount Drive"
+	echo "3. Install ArchLinux"
+	echo "4. Change Root"
+	echo "5. Configure"
+	echo "6. Exit"
 
 	while true; do
 		echo
-		read -p 'Select an option: ' option
+		read -p "Select an option: " option
 		echo
 		[[ $option =~ ^[1-6]$ ]] && break
-		echo 'Error: Invalid option.'
+		echo "Error: Invalid option."
 	done
 
 	case $option in
